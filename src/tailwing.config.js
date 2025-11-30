4b02ac3a390102576a5a1db4cd61c61a1a716ac4
Sergio Ruiz <serruiz22@gmail.com>
2025-11-30 13:30:04 -0700

    chat test 5

--- src/tailwing.config.js
+++ src/tailwing.config.js
@@ -1,5 +1,12 @@
-module.exports = {
-content: ['./index.html', './src/**/*.{js,jsx}'],
-theme: { extend: {} },
-plugins: []
-}
+/** @type
+ * {import('tailwindcss').Config} */
+export default {
+    content: [ 
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {}
+    },
+    plugins:[]
+    }
