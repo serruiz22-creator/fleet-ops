ce2c4e5b31c7884d261d9af1c526cb2ddf60dbca
Sergio Ruiz <serruiz22@gmail.com>
2025-11-30 12:56:23 -0700

    chat test 2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-plugins: [react()],
-build: { sourcemap: true }
-}
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Fleet Ops',
+        short_name: 'FleetOps',
+        display: 'standalone',
+        start_url: '/',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: '/icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/maskable-icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      }
+    })
+  ]
+})
